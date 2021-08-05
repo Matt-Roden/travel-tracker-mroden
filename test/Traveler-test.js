d9ac6623011d8cffc0fef7f6ccd85eae895c9bfa
Matt Roden <mattroden92@gmail.com>
2021-08-05 14:13:44 -0600

    Normalize and start traveler test file

---
+++ test/Traveler-test.js
@@ -0,0 +1,16 @@
+import chai from 'chai';
+const expect = chai.expect;
+import Traveler from '../src/Traveler'
+import Destination from '../src/Destination'
+import Trip from '../src/Trip'
+import sampleData from './test-data'
+
+describe('Traveler', () => {
+  let destinations, trips, traveler1, traveler2, traveler3
+
+  beforeEach(() => {
+    destinations = newDestination
+  })
+
+
+});
