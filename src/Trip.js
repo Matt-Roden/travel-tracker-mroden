b4c676ee213186f86f6de336a02fa95f66cd0859
Matt Roden <mattroden92@gmail.com>
2021-08-05 15:01:23 -0600

    Create Trip class base

---
+++ src/Trip.js
@@ -0,0 +1,26 @@
+class Trip {
+  constructor(trip, destinations) {
+    this.id = trip.id;
+    this.userID = trip.userID;
+    this.destinationID = trip.destinationID;
+    this.travelersAmount = trip.travelers;
+    this.startDate = trip.date;
+    this.duration = trip.duration;
+    this.status = trip.status || 'pending';
+    this.suggestedActivities = trip.suggestedActivities;
+    this.destinationName = destionations.findDestination(trip.destinationID).destination;
+    this.dailyLodgingCost = destinations.findDestination(trip.destinationID).estimatedLodgingCostPerDay;
+    this.flightCostPerTraveler = destinations.findDestination(trip.destinationID).estimatedFlightCostPerPerson;
+    this.picture = destinations.findDestination(trip.destinationID).image;
+  }
+  calculateTripCost() {
+    //1) ((this.dailyLodgingCost + this.flightCostPerTraveler) * this.travelersAmount) + 10% fee
+    // will probably need to make a variable that holds the 10% fee amount after getting initial pre-fee cost
+    // can then add this value to the output of the cost without the fee and return this number
+  }
+  determineTripStatus() {
+    // if the status is not pending and the trip date is less than today's date - status = past
+    // if the status is not pending and the trip date is equal to todays date - status = current
+  }
+}
+export default Trip;
