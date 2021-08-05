c100b32f2686a2e90be178d99503fbe285b29ca9
Matt Roden <mattroden92@gmail.com>
2021-08-05 14:44:25 -0600

    Create Destinations class base

---
+++ src/Destination.js
@@ -0,0 +1,9 @@
+class Destination {
+  constructor(destinationsData) {
+    this.destinations = destinationsData;
+  }
+  findDestination() {
+    //will find a destination by it's ID property
+  }
+}
+export default Destination;
