3adb4d07eec38b21c71e88a3a454fc853dab157f
Matt Roden <mattroden92@gmail.com>
2021-08-05 16:16:26 -0600

    Write and pass all Destinations tests

--- test/Destination-test.js
+++ test/Destination-test.js
@@ -35,27 +35,34 @@ let destinationsData = sampleData.destinations;
   })
 
   it('should have an ID for each destination', () => {
-
+    expect(destinations.findDestination(1).id).to.equal(1);
+    expect(destinations.findDestination(2).id).to.equal(2);
   });
 
   it('should have a city for each destination', () => {
-
+    expect(destinations.findDestination(1).destination).to.equal('Lima, Peru');
+    expect(destinations.findDestination(2).destination).to.equal('Stockholm, Sweden');
   });
 
   it('should have an estimated cost for lodging for each destination', () => {
-
+    expect(destinations.findDestination(1).estimatedLodgingCostPerDay).to.equal(70);
+    expect(destinations.findDestination(2).estimatedLodgingCostPerDay).to.equal(100);
   });
 
   it('should have an estimated cost per person for flights for each destination', () => {
-
+    expect(destinations.findDestination(1).estimatedFlightCostPerPerson).to.equal(400);
+    expect(destinations.findDestination(2).estimatedFlightCostPerPerson).to.equal(780);
   });
 
   it('should have an image for each destination', () => {
+    expect(destinations.findDestination(1).image).to.equal('https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80');
+    expect(destinations.findDestination(2).image).to.equal('https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
 
   });
 
   it('should have alt text for each destination', () => {
-
+    expect(destinations.findDestination(1).alt).to.equal('overview of city buildings with a clear sky');
+    expect(destinations.findDestination(2).alt).to.equal('city with boats on the water during the day time');
   });
 
 }); //END TEST
