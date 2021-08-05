3da0b773f0ba4d223fc975fe2598ffa9a7c10858
Matt Roden <mattroden92@gmail.com>
2021-08-05 15:21:48 -0600

    Write all it statements

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -6,11 +6,49 @@ import Trip from '../src/Trip'
 import sampleData from './test-data'
 
 describe('Traveler', () => {
-  let destinations, trips, traveler1, traveler2, traveler3
+  let destinations, allTrips, traveler1, traveler2
 
   beforeEach(() => {
-    destinations = newDestination
+    destinations = new Destination(sampleData.destinations);
+    allTrips = samppleData.trips.map((trip) => new Trip(trip, destinations));
+    traveler1 = new Traveler(sampleData.travelers[0], trips);
+    traveler2 = new Traveler(sampleData.travelers[1], trips);
+  });
+
+  it('should be a function', () => {
+    
+  });
+
+  it('should have an ID', () => {
+
   })
 
+  it('should have a name', () => {
+
+  })
+
+  it('should have a type', () => {
+
+  })
+
+  it('should know all past trips', () => {
+
+  })
+
+  it('should know all upcoming trips', () => {
+
+  })
+
+  it('should know all current trips', () => {
+
+  })
+
+  it('should know all pending trips', () => {
+
+  })
+
+  it('should calculate the total spent on trips this year', () => {
+
+  })
 
 });
