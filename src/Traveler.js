5f30546b325767d5e39dbea63a7b7a6ee4209c6b
Matt Roden <mattroden92@gmail.com>
2021-08-05 18:16:57 -0600

    Pass all props tests

--- src/Traveler.js
+++ src/Traveler.js
@@ -3,10 +3,10 @@ class Traveler {
     this.id = traveler.id;
     this.name = traveler.name;
     this.type = traveler.travelerType;
-    this.pastTrips = []; //will be a filtered array of all trip objects that have the same userID as this traveler and the date is prior to today's date
-    this.upcomingTrips = [] // will be filtered array of all trip objects that have the same userID as this traveler and the date is later than today's date
-    this.currentTrips = [] // will be filtered array of all trip objects that have the same userID as this traveler and the date is the same as today's date
-    this.pendingTrips = [] // will be filtered array of all trip objects that have the same userID as this traveler and the status is pending
+    this.trips = tripsData.filter((trip) => trip.userID === this.id); //will be a filtered array of all trip objects that have the same userID as this traveler and the date is prior to today's date
+    // this.upcomingTrips = tripsData.filter((trip) => trip.id === this.id && trip.status === 'pending');// will be filtered array of all trip objects that have the same userID as this traveler and the date is later than today's date
+    // this.currentTrips = [] // will be filtered array of all trip objects that have the same userID as this traveler and the date is the same as today's date
+    // this.pendingTrips = [] // will be filtered array of all trip objects that have the same userID as this traveler and the status is pending
   }
   calculateTotalSpentThisYear() {
     // will filter all of the current, past, and upcoming trips for this traveler that are in 2021, and then add the costs up + 10% fee
