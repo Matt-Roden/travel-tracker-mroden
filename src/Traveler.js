08622fb2971cda3e1325dccf2592167c737cf31a
Matt Roden <mattroden92@gmail.com>
2021-08-09 02:22:33 -0600

    delete comments

--- src/Traveler.js
+++ src/Traveler.js
@@ -3,8 +3,7 @@ class Traveler {
     this.id = traveler.id;
     this.name = traveler.name;
     this.type = traveler.travelerType;
-    this.trips = tripsData.filter((trip) => trip.travelerID === this.id); //will be a filtered array of all trip objects that have the same userID as this traveler and the date is prior to today's date
-    //FIX 👇
+    this.trips = tripsData.filter((trip) => trip.travelerID === this.id);
     this.upcomingTrips = this.trips.reduce((accArr, trip) => {
       if(trip.determineTripStatus(trip.startDate)) {
         accArr.push(trip)
@@ -29,12 +28,6 @@ class Traveler {
       }
       return accArr
     }, []);
-    //((trip) => trip.determineTripStatus(trip.startDate));// will be filtered array of all trip objects that have the same userID as this traveler and the date is later than today's date
-    // this.currentTrips = this.trips.forEach((trip) => trip.determineTripStatus(trip.startDate)); // will be forEached array of all trip objects that have the same userID as this traveler and the date is the same as today's date
-    // this.pendingTrips = this.trips.forEach((trip) => trip.determineTripStatus(trip.startDate));
-    // this.pastTrips = this.trips.forEach((trip) => trip.determineTripStatus(trip.startDate)); // will be filtered array of all trip objects that have the same userID as this traveler and the status is pending
-    //need to fix these 👆 coming back as undefined
-    //don't seem to be assigning the right status, need to comb through data again. WHY???
   }
   calculateTotalSpentThisYear(year) {
     const nonPendingTrips = this.trips.filter((trip) => trip.status !== 'pending');
