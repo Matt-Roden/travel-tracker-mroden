ea490b6762844bf314040df6db0afa626c0de87a
Matt Roden <mattroden92@gmail.com>
2021-08-09 02:21:35 -0600

    Add reduces to the traveler constructor - pass final test

--- src/Traveler.js
+++ src/Traveler.js
@@ -5,11 +5,36 @@ class Traveler {
     this.type = traveler.travelerType;
     this.trips = tripsData.filter((trip) => trip.travelerID === this.id); //will be a filtered array of all trip objects that have the same userID as this traveler and the date is prior to today's date
     //FIX 👇
-    this.upcomingTrips = this.trips.forEach((trip) => trip.determineTripStatus(trip.date));// will be filtered array of all trip objects that have the same userID as this traveler and the date is later than today's date
-    this.currentTrips = this.trips.forEach((trip) => trip.determineTripStatus(trip.date)); // will be forEached array of all trip objects that have the same userID as this traveler and the date is the same as today's date
-    this.pendingTrips = this.trips.forEach((trip) => trip.determineTripStatus(trip.date));
-    this.pastTrips = this.trips.forEach((trip) => trip.determineTripStatus(trip.date)); // will be filtered array of all trip objects that have the same userID as this traveler and the status is pending
+    this.upcomingTrips = this.trips.reduce((accArr, trip) => {
+      if(trip.determineTripStatus(trip.startDate)) {
+        accArr.push(trip)
+      }
+      return accArr
+    }, []);
+    this.pastTrips = this.trips.reduce((accArr, trip) => {
+      if(trip.determineTripStatus(trip.startDate)) {
+        accArr.push(trip)
+      }
+      return accArr
+    }, []);
+    this.currentTrips = this.trips.reduce((accArr, trip) => {
+      if(trip.determineTripStatus(trip.startDate)) {
+        accArr.push(trip)
+      }
+      return accArr
+    }, []);
+    this.pendingTrips = this.trips.reduce((accArr, trip) => {
+      if(trip.determineTripStatus(trip.startDate)) {
+        accArr.push(trip)
+      }
+      return accArr
+    }, []);
+    //((trip) => trip.determineTripStatus(trip.startDate));// will be filtered array of all trip objects that have the same userID as this traveler and the date is later than today's date
+    // this.currentTrips = this.trips.forEach((trip) => trip.determineTripStatus(trip.startDate)); // will be forEached array of all trip objects that have the same userID as this traveler and the date is the same as today's date
+    // this.pendingTrips = this.trips.forEach((trip) => trip.determineTripStatus(trip.startDate));
+    // this.pastTrips = this.trips.forEach((trip) => trip.determineTripStatus(trip.startDate)); // will be filtered array of all trip objects that have the same userID as this traveler and the status is pending
     //need to fix these 👆 coming back as undefined
+    //don't seem to be assigning the right status, need to comb through data again. WHY???
   }
   calculateTotalSpentThisYear(year) {
     const nonPendingTrips = this.trips.filter((trip) => trip.status !== 'pending');
