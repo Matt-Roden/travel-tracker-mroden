5bd35b65d14f117c9f868fb049c8c99d68958744
Matt Roden <mattroden92@gmail.com>
2021-08-08 19:17:45 -0600

    Begin displayAllDestinations function

---
+++ src/updateDOM.js
@@ -0,0 +1,16 @@
+import { destinations, allTrips, traveler } from './scripts.js'
+
+var dayjs = require('dayjs')
+
+const todaysDate = (dayjs().format('YYYY/MM/DD'))
+console.log(todaysDate, 'todaybutt');
+
+
+
+const displayAllData = () => {
+  displayAllDestinations();
+};
+
+const displayAllDestinations = () => {
+
+}
